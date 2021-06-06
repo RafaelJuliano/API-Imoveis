c9193a5a2e0dce445b0da44ffa56ba55446d5e8c
Rafael Juliano Ferreira <67478757+RafaelJuliano@users.noreply.github.com>
2021-06-06 14:51:01 -0300

    comentários

--- src/controllers/properties.js
+++ src/controllers/properties.js
@@ -1,4 +1,3 @@
-const Property = require('../models/properties');
 const Status = require('http-status');
 const service = require('../services/properties');
 
@@ -15,16 +14,17 @@ exports.getProperty = async (request, response, next) => {
     const id = request.params.id;
 
     try {
-        const property = await service.getProperty(id);
-    if (property) {
-        response.send(property);
-    } else {
-        response.status(Status.NOT_FOUND).send();
-    };
+        const propertyFound = await service.getProperty(id);
+        if (propertyFound) {
+            response.send(propertyFound);
+        } else {
+            response.status(Status.NOT_FOUND).send();
+        };
     } catch (error) {
         next(error);
-    }    
+    }
 };
+
 
 /*
 Solicita todos os imóveis cadastrados. Os resultados são paginados para melhor performace.
@@ -35,22 +35,24 @@ exports.getProperties = async (request, response, next) => {
     const limit = parseInt(request.query.limit || 0);
     const page = parseInt(request.query.page || 0);
 
+    //limit e page dever ser inteiros.
     if (!Number.isInteger(limit) || !Number.isInteger(page)) {
         response.status(Status.BAD_REQUEST).send();
     };
 
     try {
-        const properties = await service.getProperties(limit, page);
+        const propertiesFound = await service.getProperties(limit, page);
 
-    if (properties && properties.length) {
-        response.send(properties);
-    } else {
-        response.status(Status.NOT_FOUND).send();
-    };
+        if (propertiesFound && propertiesFound.length) {
+            response.send(propertiesFound);
+        } else {
+            response.status(Status.NOT_FOUND).send();
+        };
     } catch (error) {
         next(error);
-    }    
+    }
 };
+
 
 /*
 Solicita a inclusão de um novo imóvel no banco de dados.
@@ -58,7 +60,7 @@ Solicita a inclusão de um novo imóvel no banco de dados.
 @paran number = Recebe inteiro com limite de 6 digitos.
 @paran complemente = (Opcional).
 @paran price = Recebe um decimal com limite de 10 digitos.
-@rooms = Recebe um inteiro com limit de 2 digitos.
+@paran rooms = Recebe um inteiro com limit de 2 digitos.
 */
 exports.registerNewProperty = async (request, response, next) => {
     try {
@@ -69,35 +71,35 @@ exports.registerNewProperty = async (request, response, next) => {
             price: request.body.price,
             rooms: request.body.rooms
         };
-    
+
         const newProperty = await service.registerNewProperty(property);
         response.status(Status.CREATED).send(newProperty);
     } catch (error) {
         next(error);
-    }    
+    }
 }
 
 
 /*
 Solicita a alteração de uma ou mais colunas de um imóvel.
 @paran id = Id do imóvel a ser requisitado via url.
-paran cep = Deve receber no formáto 000.000.000-00.
+@paran cep = Deve receber no formáto 000.000.000-00.
 @paran number = Recebe inteiro com limite de 6 digitos.
-@paran complemente = (Opcional).
+@paran complemento = (Opcional).
 @paran price = Recebe um decimal com limite de 10 digitos.
-@rooms = Recebe um inteiro com limit de 2 digitos.
+@paran rooms = Recebe um inteiro com limit de 2 digitos.
 */
 exports.updateProperty = async (request, response, next) => {
     try {
         const property = {
-            id : request.params.id,
+            id: request.params.id,
             cep: request.body.cep,
             number: request.body.number,
             complement: request.body.complement,
             price: request.body.price,
             rooms: request.body.rooms
         };
-    
+
         const result = await service.updateProperty(property);
         if (result == 'updated') {
             response.status(Status.OK).send()
@@ -106,7 +108,7 @@ exports.updateProperty = async (request, response, next) => {
         }
     } catch (error) {
         next(error)
-    }   
+    }
 }
 
 
@@ -117,15 +119,14 @@ Solicita a exclusão de um imóvel.
 exports.deleteProperty = async (request, response, next) => {
     const id = request.params.id
 
-    Property.findByPk(id).then((property) => {
-        if (property) {
-            Property.destroy({
-                where: { id: id }
-            }).then(() => {
-                response.send()
-            }).catch((error) => next(error))
-        } else {
+    try {
+        const result = await service.deleteProperty(id);
+        if (result == 'deleted') {
+            response.status(Status.OK).send()
+        } else if (result == 'not found') {
             response.status(Status.NOT_FOUND).send()
         }
-    }).catch((error) => next(error))
+    } catch (error) {
+        next(error)
+    }
 }
--- src/services/properties.js
+++ src/services/properties.js
@@ -1,14 +1,32 @@
 const Property = require('../models/properties');
-const Status = require('http-status');
 
+/*
+PROPERTY SERVICE LAYER
+Responsável por executar a regra de negócio e invocar o objeto Sequelize para operações no banco de dados.
+*/
+
+/*
+Retorna um imóvel pelo seu ID
+@paran id = Valor inteiro com id do imóvel.
+@return = Imóvel encontrado.
+*/
 exports.getProperty = async (id) => {
     const property = await Property.findByPk(id);
     return property;
 }
 
+/*
+Retorna a quantidade (limit) do intervalo (page) dos imóveis cadastrados ordenados pelo id;
+exemplo. (page = 2, limit = 10) Retorna intervalo entre 21 e 30.
+@paran limit = Tamanho do intervalo retornado.
+@paran page = Determina o intervalo retornado.
+@return = Array de objetos com imóveis.
+*/
 exports.getProperties = async (setLimit, setPage) => {
     let limit = setLimit;
     let page = setPage;
+
+    //Impede que o limite seja maior que 20 ou negativo
     const ITENS_PER_PAGE = 20;
     limit = limit > ITENS_PER_PAGE || limit <= 0 ? ITENS_PER_PAGE : limit;
     page = page <= 0 ? 0 : page * limit;
@@ -17,6 +35,11 @@ exports.getProperties = async (setLimit, setPage) => {
     return properties;
 };
 
+/*
+Cadastra um novo imóvel.
+@paran property = Objeto com dados do imóvel.
+@return = Objeto do imóvel cadastrado.
+*/
 exports.registerNewProperty = async (property) => {
     const newProperty = await Property.create({
         cep: property.cep,
@@ -28,17 +51,39 @@ exports.registerNewProperty = async (property) => {
     return newProperty;
 }
 
-exports.updateProperty = async (property, next) => {
+
+/*
+Atualiza um ou mais dados de um imóvel.
+@paran property = Objeto com dados a serem atualizados do imóvel. ID é obrigatório.
+@return = String com confirmação se o imóvel foi atualizado ou não encontrado.
+*/
+exports.updateProperty = async (property) => {
     const propertyExist = await Property.findByPk(property.id);
     if (propertyExist) {
-        const result = await Property.update({
+        const updated = await Property.update({
             number: property.number,
             cep: property.cep,
             complement: property.complement,
             price: property.price,
             rooms: property.rooms
         }, { where: { id: property.id } });
-        return 'updated';
+        if (updated) return 'updated';
+    } else {
+        return 'not found';
+    }
+}
+
+
+/*
+Deleta um imóvel.
+@paran id = Id do imóvel a ser deletado.
+@return = String com confirmação se o imóvel foi deletado ou não encontrado.
+*/
+exports.deleteProperty = async (id) => {
+    const propertyExist = await Property.findByPk(id);
+    if (propertyExist) {
+        const deleted = await Property.destroy({ where: { id: id } });
+        if(deleted)  return 'deleted';
     } else {
         return 'not found';
     }
