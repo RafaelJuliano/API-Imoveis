cee6f416090dc0d3aa20a4159b5be9b246bd0166
Rafael Juliano Ferreira <67478757+RafaelJuliano@users.noreply.github.com>
2021-06-09 12:23:17 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
 	development: {
 		database: {
-			host: process.env.DB_HOST_REMOTE,
-			port: process.env.DB_PORT_REMOTE,
-			name: process.env.DB_NAME_REMOTE,
-			dialect: process.env.DB_DIALECT_REMOTE,
-			user: process.env.DB_USER_REMOTE,
-			password: process.env.DB_PASSWORD_REMOTE
+			host: 'localhost',
+			port: 3306,
+			name: 'imoveisapi',
+			dialect: 'mysql',
+			user: 'root',
+			password: ''
 		}
 	},
 	
