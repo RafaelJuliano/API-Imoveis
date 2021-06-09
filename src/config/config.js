0987d498ade8b907b41d3a3860deeb4d1f408ac3
Rafael Juliano Ferreira <67478757+RafaelJuliano@users.noreply.github.com>
2021-06-09 11:31:15 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -12,8 +12,12 @@ module.exports = {
 	
 	production: {
 		database: {
-			host: process.env.DB_HOST,
-			port: process.env.DB_PORT
+			host: process.env.DB_HOST_REMOTE,
+			port: process.env.DB_PORT_REMOTE,
+			name: process.env.DB_NAME_REMOTE,
+			dialect: process.env.DB_DIALECT_REMOTE,
+			user: process.env.DB_USER_REMOTE,
+			password: process.env.DB_PASSWORD_REMOTE
 		}
 	}
 };
