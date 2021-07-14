03e36417915cb6e3b6a21b8fe7df9b8a84b809cb
Rafael Juliano Ferreira <67478757+RafaelJuliano@users.noreply.github.com>
2021-07-14 20:28:35 -0300

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.NODE_ENV //|| "development";
 
 const config = require("../config/config.js")[environment];
 
